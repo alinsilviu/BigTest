a48f3a4cb505f8b76efd8c95d6e93a245292441e
alinsilviu <alinsilviu@users.noreply.github.com>
2015-07-31 23:05:14 +0300

    Update login.php

--- login.php
+++ login.php
@@ -29,9 +29,23 @@
 			<hr>
 	</div>
 	<?php
-
-	include ('new.php');
-
+		$match1 = 'admin';
+		$match2 = 'admin';
+	
+		if (isset($_POST['password'])&&isset($_POST['username'])) {
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+	
+			if (!empty($password)&&!empty($username)) {
+				if ($password==$match2&&$username==$match1) {
+					header("Location: index.php");
+				} else {
+					header("Location: login.php");
+				}
+			} else {
+				header("Location: login.php");
+			}
+		}
 	?>
 	<script>
 		$(document).ready(function(){
